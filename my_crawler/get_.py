fed5fd40937522bfa2c947c08512c9e88c0d4f64
manengyu@qq.com <manengyu@qq.com>
2017-08-03 18:05:00 +0800

    change

--- my_crawler/get_.py
+++ my_crawler/get_.py
@@ -60,10 +60,7 @@ def climb_tianyan(sheet, file_name):
     sheet_w = new_workbook.add_sheet(sheet)
     yesterday = str(datetime.datetime.now().month) + '月' + str(datetime.datetime.now().day - 1) + '日'
     plat = [
-        "汇理财",
-"麻袋理财",
-"易通贷",
-"民贷天下",
+
 ]
     r = requests.session()
     r.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0, WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",}
@@ -84,8 +81,46 @@ def climb_tianyan(sheet, file_name):
     new_workbook.save(file_name)
 
 
-def get_info():
+def climb_hujing(conn, cur, url):
+    x = lxml.html.fromstring(r.get(url).text)  # .replace(' ', '').replace('\n', '')
+    plat_id = url.lstrip('')
+    plat_name = x.xpath('//*[@id="theForm"]/div[1]/div[2]/div[1]/div[1]/span[1]/text()')[0]
+    tr = x.xpath('//*[@id="trade-log"]/table[1]/tr')
+    table2_td = x.xpath('//*[@id="trade-log"]/table[2]/tr[2]/td')
+    for i in range(2, len(tr) + 1):
+        data_ls = []
+        day_date = x.xpath('//*[@id="trade-log"]/table[1]/tr[' + str(i) + ']/td/text()')[0]
+        create_time = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+        for j in range(1, len(table2_td) + 1):
+            try:
+                value = x.xpath('//*[@id="trade-log"]/table[2]/tr[' + str(i) + ']/td[' + str(j) + ']/text()')[0]  # .replace('\r\n', '').replace('\n', '').replace('\t', '').replace(' ', '')
+                if len(value):
+                    data_ls.append('%.2f' % float(value))
+                else:  # 为空
+                    data_ls.append(value)
+            except:
+                # traceback.print_exc()
+                data_ls.append('0.00')
+                logging.warning(u'表格不足默认值设为0.00：%s,%s,%s,%s', plat_name, url, i, j)
+        [data_ls.append('0.00') for i in range(len(data_ls), 27)]
+        insert_finace_association_of_china(conn, cur, plat_id, plat_name, day_date, data_ls[0], data_ls[1], data_ls[2],
+                                           data_ls[3].rstrip('.00'), data_ls[4].rstrip('.00'), data_ls[5], data_ls[6], data_ls[7], data_ls[8],
+                                           data_ls[9], data_ls[10], data_ls[11], data_ls[12], data_ls[13],
+                                           data_ls[14], data_ls[15], data_ls[16], data_ls[17], data_ls[18],
+                                           data_ls[19], data_ls[20], data_ls[21], data_ls[22], data_ls[23],
+                                           data_ls[24], data_ls[25], data_ls[26], url, create_time)
 
+
+def get_info():
+    first_url = ''
+    x = lxml.html.fromstring(r.get(first_url).text)
+    total_nu = x.xpath('//*[@id="oldpage"]/a[13]/@href')[0].lstrip('')
+    # select_sql = "SELECT plat_name FROM finace_association_of_china"
+    # app_management = pd.read_sql(select_sql, conn_yuqing)
+    for i in range(1, int(total_nu) + 1):
+        url_page = '' + str(i)
+        for j in lxml.html.fromstring(r.get(url_page).text).xpath('//*[@id="runinfotbody"]/tr/td[8]/a/@href'):
+            climb_hujing(conn_yuqing, cur, '' + j)
 
 # 测试函数
 if __name__ == "__main__":
@@ -109,8 +144,8 @@ if __name__ == "__main__":
         # 'Accept-Language': 'zh-CN,zh;q=0.8'
     }
     name_url = get_company_url(conn_yuqing)
-    error_plat = u"上海诺诺镑客金融信息服务有限公司"  # 默认从第一条运行
-    # error_plat = u"贵州国信通电子商务有限公司"
+    error_plat = u""  # 默认从第一条运行
+    # error_plat = u""
     nu = 0
     while len(error_plat):
         time.sleep(1)
