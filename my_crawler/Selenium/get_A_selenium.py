0ef8ab3f6ea78477b1b98aaba9b3e4c9d66993fc
Nengyu Ma <manengyu@qq.com>
2019-03-05 17:05:18 +0800

    add download blob url

--- my_crawler/Selenium/get_A_selenium.py
+++ my_crawler/Selenium/get_A_selenium.py
@@ -1,6 +1,8 @@
 # -*-coding:utf-8-*-
 import time
+import base64
 from selenium import webdriver
+from urllib.parse import quote
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
@@ -86,3 +88,70 @@ class Gsxt(object):
 
 # if __name__ == u"__main__":
 #     Gsxt("chrome").run(multi_type, page_total, url)
+
+# get blob url
+def get_file_content_chrome(driver, uri):
+    result = driver.execute_async_script("""
+    var uri = arguments[0];
+    var callback = arguments[1];
+    var toBase64 = function(buffer){for(var r,n=new Uint8Array(buffer),t=n.length,a=new Uint8Array(4*Math.ceil(t/3)),i=new Uint8Array(64),o=0,c=0;64>c;++c)i[c]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".charCodeAt(c);for(c=0;t-t%3>c;c+=3,o+=4)r=n[c]<<16|n[c+1]<<8|n[c+2],a[o]=i[r>>18],a[o+1]=i[r>>12&63],a[o+2]=i[r>>6&63],a[o+3]=i[63&r];return t%3===1?(r=n[t-1],a[o]=i[r>>2],a[o+1]=i[r<<4&63],a[o+2]=61,a[o+3]=61):t%3===2&&(r=(n[t-2]<<8)+n[t-1],a[o]=i[r>>10],a[o+1]=i[r>>4&63],a[o+2]=i[r<<2&63],a[o+3]=61),new TextDecoder("ascii").decode(a)};
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(){ callback(toBase64(xhr.response)) };
+    xhr.onerror = function(){ callback(xhr.status) };
+    xhr.open('GET', uri);
+    xhr.send();
+    """, uri)
+    if type(result) == int:
+        raise Exception("Request failed with status %s" % result)
+    return base64.b64decode(result)
+
+url_suffix = "test-"
+if getattr(config, 'ENV', None) and config.ENV.lower() in [u"prod", u"production"]:
+    url_suffix = ""
+filename = ""
+url_selenium = ""
+url = ""
+chrome_options = webdriver.ChromeOptions()
+prefs = {
+    # "download.prompt_for_download": True,
+    # 'download.default_directory': os.getcwd(),
+    # "plugins.always_open_pdf_externally": True,
+    # 'profile.default_content_settings.popups': 0,  # 设置为0，禁止弹出窗口
+}
+chrome_options.add_experimental_option('prefs', prefs)
+driver = webdriver.Remote(command_executor=url_selenium, desired_capabilities=chrome_options.to_capabilities())
+driver.get(url_report)
+now_time = time.time()
+while True:
+    if time.time() - now_time > 10: break
+    try:
+	element = driver.find_element_by_id("")
+	filename = element.get_attribute("")
+	element.click()
+	break
+    except WebDriverException:
+	time.sleep(1)
+now_time = time.time()
+while True:
+    if time.time() - now_time > 10: break
+    try:
+	driver.switch_to.window(driver.window_handles[-1])
+	break
+    except WebDriverException:
+	time.sleep(1)
+now_time = time.time()
+while True:
+    if time.time() - now_time > 60: break
+    try:
+	element = driver.find_element_by_xpath('//*[@id=""]')
+	driver.get(element.get_property("src"))
+	driver.get_screenshot_as_file("img1.png")
+	break
+    except WebDriverException:
+	time.sleep(2)
+rep_bytes = get_file_content_chrome(driver, driver.current_url)
+driver.close()
+driver.quit()
+return rep_bytes, 200, {'Content-Type': "application/pdf",
+                                   "Content-Disposition": f"attachment;filename*=UTF-8''{quote(filename)}.pdf"}
