5c514ec13fc60e9dbf24fe7927588f72721f9461
Nengyu Ma <manengyu@qq.com>
2020-08-05 10:58:16 +0800

    Update Cookie.py

--- my_crawler/mycrawler_moban/Cookie.py
+++ my_crawler/mycrawler_moban/Cookie.py
@@ -6,14 +6,30 @@ from random import randint
 
 
 tmp = ""
+cookie = ""
 uid = str(randint(0, 255))
 m1 = md5()
 m1.update(uid.encode())
 uid = m1.hexdigest()
-Degrees.cookie["JSESSIONID"] = uid.upper()
-Degrees.cookie["Hm_lvt_"] = str(int(time.time()))
-Degrees.cookie["Hm_lpvt_"] = str(int(time.time()))
+cookie["JSESSIONID"] = uid.upper()
+cookie["Hm_lvt_"] = str(int(time.time()))
+cookie["Hm_lpvt_"] = str(int(time.time()))
 for k, v in A.items():
     tmp += f"{k}={v};"
 tmp = tmp.strip(";")
-reqs.headers["cookie"] = tmp
+//cannot use reqs.headers = headers
+r = reqs.get(url, headers={"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                            "Accept-Encoding":"gzip, deflate",
+                            "Accept-Language":"zh-CN,zh;q=0.9",
+                            "Cache-Control":"max-age=0",
+                            "Content-Length":"227",
+                            "Content-Type":"application/x-www-form-urlencoded",
+                            "Host":"",
+                            "Origin":"",
+                            # "Proxy-Connection":"keep-alive",
+                            "Connection":"keep-alive",
+                            "Referer":"",
+                            "Upgrade-Insecure-Requests":"1",
+                            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+                            "cookie": tmp,
+                            })
