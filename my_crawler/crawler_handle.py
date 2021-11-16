12a7e385bb8ae51c9f11e5e41a6b316d34dbfd92
Nengyu Ma <manengyu@qq.com>
2021-11-16 11:28:19 +0800

    Update crawler_handle.py

--- my_crawler/crawler_handle.py
+++ my_crawler/crawler_handle.py
@@ -3,5 +3,11 @@ a = """"""
 import json
 tmp = {}
 for i in a.split("\n"):
-    tmp[i.split(":")[0].strip()] = i.split(":")[1].strip()
+    tmp[i.split(": ")[0].strip()] = i.split(": ")[1].strip()
+print(json.dumps(tmp, indent=True))
+
+# POST params
+tmp = {}
+for i in query.split("&"):
+    tmp[i.split("=")[0].strip()] = i.split("=")[1].strip()
 print(json.dumps(tmp, indent=True))
